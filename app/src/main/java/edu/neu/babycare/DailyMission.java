2127b83d83f770ae30388c4076084bf09cc1bf73
Shawn (Qiyang) Xie <qiyangx0714@gmail.com>
2022-04-28 23:06:48 -0700

    Added checkbox to DailyMission.java

--- app/src/main/java/edu/neu/babycare/DailyMission.java
+++ app/src/main/java/edu/neu/babycare/DailyMission.java
@@ -3,6 +3,8 @@ package edu.neu.babycare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class DailyMission extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class DailyMission extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_mission);
+
+    }
+
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
     }
 }
